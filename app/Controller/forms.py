7d2819a6950b72ca7236929ee9b74acb841c48c4
wendaliu <321wenda@gmail.com>
2021-09-13 16:05:06 -0700

    version v.01 - revdised html and revised post.model

--- app/Controller/forms.py
+++ app/Controller/forms.py
@@ -6,5 +6,7 @@ from app.Model.models import Post
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
+    body = TextAreaField('Post message',validators =[Length(min=1,max=1500)])
     happiness_level = SelectField('Happiness Level',choices = [(3, 'I can\'t stop smiling'), (2, 'Really happy'), (1,'Happy')])
     submit = SubmitField('Post')
+
