31f22cd8af0b99687432efc1ef91d8cbe2bb8953
wendaliu <321wenda@gmail.com>
2021-09-15 20:34:39 -0700

    Version V.03 - add the like button and have the total post set up

--- app/Controller/routes.py
+++ app/Controller/routes.py
@@ -16,13 +16,23 @@ bp_routes.template_folder = Config.TEMPLATE_FOLDER #'..\\View\\templates'
 @bp_routes.route('/index', methods=['GET'])
 def index():
     posts = Post.query.order_by(Post.timestamp.desc())
-    return render_template('index.html', title="Smile Portal", posts=posts.all())
+    return render_template('index.html', title="Smile Portal", posts=posts.all(), total_smile= posts.count())
 
-@bp_routes.route('/likes/', methods = ['GET'])
-def likes ():
-    likes = Post.query.order_by(Post.likes.desc())
-    return render_template('_post.html', likes=likes)
+#@bp_routes.route('/likes/', methods = ['GET'])
+#def likes ():
+#    likes = Post.query.order_by(Post.likes.desc())
+#    return render_template('_post.html', likes=likes)
 
+@bp_routes.route('/likes/<post_id>', methods =['POST'])
+def likes(post_id):
+    post = Post.query.filter_by(id=post_id).first()
+    if post:
+        post.likes +=1
+        db.session.add(post)
+        db.session.commit()
+    return redirect(url_for('routes.index'))
+
+#code was gotten from flask exercise
 @bp_routes.route('/smile/', methods = ['GET' , 'POST'])
 def smile ():
     pform = PostForm()
@@ -30,6 +40,10 @@ def smile ():
         newpost = Post(title = pform.title.data, body = pform.body.data, happiness_level= pform.happiness_level.data)
         db.session.add(newpost)
         db.session.commit()
-        flash('New smile ' + newpost.title)
+        flash('New Post ' + newpost.title)
         return redirect(url_for('routes.index'))
     return render_template('create.html', form = pform)
+
+
+
+    
